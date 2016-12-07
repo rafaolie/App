4b412be8b51ce18968846ebd6c8f60ff59c34f09
Rafa <rafael.r.oliveira@hotmail.com>
2016-12-06 22:47:01 -0200

    Botao linha/bloco adicionado

---
+++ principal.js
@@ -0,0 +1,10 @@
+document.querySelector("#mudaLayout").addEventListener("click", function(){
+  var mural = document.querySelector(".mural");
+  mural.classList.toggle("mural--linhas");
+
+  if (mural.classList.contains("mural--linhas")) {
+    this.textContent = "Blocos";
+  }else {
+    this.textContent = "Linhas";
+  }
+});
