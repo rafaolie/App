d87f82bf0e383d23258f95beaf1b8bdbf01630dd
Rafa <rafael.r.oliveira@hotmail.com>
2016-12-07 19:28:12 -0200

    Botoes excluir adicionados

--- principal.js
+++ principal.js
@@ -8,3 +8,19 @@ document.querySelector("#mudaLayout").addEventListener("click", function(){
     this.textContent = "Linhas";
   }
 });
+
+function removeCartao() {
+  var cartao = document.querySelector("#cartao_" + this.getAttribute("data-ref"));
+
+  cartao.classList.add("cartao--some");
+
+  setTimeout(function() {
+    cartao.remove();
+  },400);
+}
+
+var botoes = document.querySelectorAll(".opcoesDoCartao-remove");
+
+for (var i = 0; i < botoes.length; i++) {
+  botoes[i].addEventListener("click", removeCartao);
+};
