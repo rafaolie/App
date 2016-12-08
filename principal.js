d5d8b3ec7633f4d89095ca1295ce735e5bcf25dd
Rafa <rafael.r.oliveira@hotmail.com>
2016-12-07 22:41:54 -0200

    O usuario crio o proprio cartao

--- principal.js
+++ principal.js
@@ -24,3 +24,32 @@ var botoes = document.querySelectorAll(".opcoesDoCartao-remove");
 for (var i = 0; i < botoes.length; i++) {
   botoes[i].addEventListener("click", removeCartao);
 };
+
+var contador = $(".cartao").length;
+$(".novoCartao").submit(function(event) {
+  var campoConteudo = $(".novoCartao-conteudo");
+  var conteudo = campoConteudo.val().trim();
+
+  if (conteudo) {
+    contador++;
+    var botaoRemove = $("<button>").addClass("opcoesDoCartao-remove")
+                                   .addClass("opcoesDoCartao-opcao")
+                                   .attr("data-ref", contador)
+                                   .text("Remover")
+                                   .click(removeCartao);
+
+    var opcoes = $("<div>").addClass("opcoesDoCartao")
+                           .append(botaoRemove);
+
+    var conteudoTag = $("<p>").addClass("cartao-conteudo")
+                              .append(conteudo);
+
+    $("<div>").attr("id", "cartao_" + contador)
+                                 .addClass("cartao").append(opcoes)
+                                 .append(conteudoTag)
+                                 .prependTo(".mural");
+  }
+
+  campoConteudo.val("");
+  event.preventDefault();
+});
