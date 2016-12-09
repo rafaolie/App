ef920e03208c57849107b53e5e931039616e29ca
Rafa <rafael.r.oliveira@hotmail.com>
2016-12-08 22:19:28 -0200

    Fonte das letras ajustadas

--- principal.js
+++ principal.js
@@ -72,12 +72,41 @@ $(".novoCartao").submit(function(event) {
     var conteudoTag = $("<p>").addClass("cartao-conteudo")
                               .append(conteudo);
 
+    var tipoCartao = decideTipoDeCartao(conteudo)
+
     $("<div>").attr("id", "cartao_" + contador)
-                                 .addClass("cartao").append(opcoes)
-                                 .append(conteudoTag)
-                                 .prependTo(".mural");
+              .addClass("cartao")
+              .addClass(tipoCartao)
+              .append(opcoes)
+              .append(conteudoTag)
+              .prependTo(".mural");
   }
 
   campoConteudo.val("");
   event.preventDefault();
 });
+
+function decideTipoDeCartao(conteudo) {
+  var quebras = conteudo.split("<br>").length;
+  var totalDeLetras = conteudo.replace(/<br>/g, " ").length;
+
+  var ultimoMaior = "";
+  conteudo.replace(/<br>/g, " ")
+          .split(" ")
+          .forEach(function (palavra) {
+            if (palavra.length > ultimoMaior.length){
+              ultimoMaior = palavra;
+            }
+          });
+
+  var tamMaior = ultimoMaior.length;
+  var tipoCartao = "cartao--textoPequeno";
+
+  if (tamMaior < 9 && quebras < 5 && totalDeLetras < 55) {
+    tipoCartao = "cartao--textoGrande"
+  } else if (tamMaior < 12 && quebras < 6 && totalDeLetras < 75) {
+    tipoCartao = "cartao--textoMedio";
+  }
+
+  return tipoCartao;
+}
